748fcbc1a46c81731471b5de9a1d91e5ea6d0fc7
ehiskuku10 <ehiskuku@gmail.com>
2019-10-09 21:15:15 +0100

    Updates

---
+++ precache-manifest.432d1beb6549731f92b06aa67287db74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7d19d2c9dd9cc3f7ae1f8fc103cba1c",
+    "url": "/Infospace/index.html"
+  },
+  {
+    "revision": "39bd05bc1ead812b9605",
+    "url": "/Infospace/static/css/main.8fd3321d.chunk.css"
+  },
+  {
+    "revision": "d61d0880b6a809f7c7ab",
+    "url": "/Infospace/static/js/2.6e348829.chunk.js"
+  },
+  {
+    "revision": "39bd05bc1ead812b9605",
+    "url": "/Infospace/static/js/main.cd3bbb72.chunk.js"
+  },
+  {
+    "revision": "c030e03716c18559fd17",
+    "url": "/Infospace/static/js/runtime~main.794c49d8.js"
+  }
+]);
