83c8f96e5800d036ffeacfbce892ed6bc3a3f1f4
ehiskuku10 <ehiskuku@gmail.com>
2019-10-09 20:53:29 +0100

    Changed Styling for home list component div

--- build/precache-manifest.c5361041d4ce7c367d2671fd6dbf34f7.js
+++ build/precache-manifest.c5361041d4ce7c367d2671fd6dbf34f7.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c2d1e7582030f71a184b0d1d3b24f018",
+    "revision": "2c7f089374cde388b0f10301ba672667",
     "url": "/Infospace/index.html"
   },
   {
-    "revision": "d536e50c0e70bd8321ea",
-    "url": "/Infospace/static/css/main.8ae2b045.chunk.css"
+    "revision": "0252c88516c7d19aaf66",
+    "url": "/Infospace/static/css/main.fff8d3f4.chunk.css"
   },
   {
     "revision": "d350ab9027380527c30d",
     "url": "/Infospace/static/js/2.c2b4dfde.chunk.js"
   },
   {
-    "revision": "d536e50c0e70bd8321ea",
-    "url": "/Infospace/static/js/main.69152ee3.chunk.js"
+    "revision": "0252c88516c7d19aaf66",
+    "url": "/Infospace/static/js/main.40f99350.chunk.js"
   },
   {
     "revision": "c030e03716c18559fd17",
