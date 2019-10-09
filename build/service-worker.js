ea2fbc2f69f8f440a5593aa0e50fbf713be47386
ehiskuku10 <ehiskuku@gmail.com>
2019-10-09 21:27:23 +0100

    Changed styling for home feed nav component: Added more css rules to media queries

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Infospace/precache-manifest.40c196edb16d653028e7b853f288cf84.js"
+  "/Infospace/precache-manifest.432d1beb6549731f92b06aa67287db74.js"
 );
 
 self.addEventListener('message', (event) => {
